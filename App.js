908bd9c8227ef423e4f52b1e8d461f7b946c88fd
Rahul Gupta <rahulgupt0711@gmail.com>
2024-04-19 03:08:56 +0530

    created folder structure

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Navigation from './CentalNavigation/Navigation';
+
+
+function App() {
+
+
+  return (
+    <>
+      <View>
+        <Navigation />
+      </View>
+    </>
+  );
+}
+
+
+export default App;
